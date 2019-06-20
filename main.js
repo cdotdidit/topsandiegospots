70bb585a807094c4699b06042aef2c042ef07a90
cash <cdotcomma@gmail.com>
2019-06-20 15:54:56 -0700

    first commit

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-$(document).ready(function() {
-// write your code here
-
-});
+$(document).ready(function($) {
+    // write your code here
+    $.getJSON( "data.json", function(data) {
+        var item = []
+        $.each(data, function (_key, val ) {
+            item.push("<tr> <td>" + val.name + "</td>" + "<td>" + val.description + "</td>" + 
+            "<td> <button id= 'buttons'><a href='https://www.google.com/maps?q=" + val.location + "'>Find Me Here!</a></button> </td> </tr>");
+        })
+        $('#table').append(item);
+    })
+    })
