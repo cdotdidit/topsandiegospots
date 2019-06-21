12c7a6f70459bfbc8ac19e6e429312291c2104f8
cash <cdotcomma@gmail.com>
2019-06-21 12:48:47 -0700

    commit of working projects sans styling

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-$(document).ready(function($) {
-    // write your code here
-    $.getJSON( "data.json", function(data) {
-        var item = []
-        $.each(data, function (_key, val ) {
-            item.push("<tr> <td>" + val.name + "</td>" + "<td>" + val.description + "</td>" + 
-            "<td> <button id= 'buttons'><a href='https://www.google.com/maps?q=" + val.location + "'>Find Me Here!</a></button> </td> </tr>");
-        })
-        $('#table').append(item);
+$(document).ready(function() {
+    $.getJSON("data.json", function(data){ 
+    var array = []
+    $.each(data, function(_key, val ){
+        array.push("<tr><td>" + val.name + "</td><td>" + val.description + "</td><td><a href =www.google.com/maps?q=" + val.location +"> Get there. </a></td></tr>");
+        console.log(array);
     })
+    $('table').append(array);
+    
+    }
+    
+    )
     })
